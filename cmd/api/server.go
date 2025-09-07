a71b585ca1b872bd6fb1d20935d6a1dc50ffcf5d
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-09-07 15:33:00 +0530

    create simple routes

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,4 +1,34 @@
 package main
 
-func main() {	
+import "net/http"
+
+func main() {
+
+	port := "3000"
+	println("Server is running on port " + port)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {	
+		if(r.Method==http.MethodGet){
+			w.Write([]byte("GET Teachers endpoint"))			
+			return
+		}	
+		if(r.Method==http.MethodPost){
+			w.Write([]byte("POST Teachers endpoint"))			
+			return
+		}
+	})
+
+	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {		
+		w.Write([]byte("Students endpoint"))			
+	})
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		panic(err)
+		
+	}
+
+
 }
