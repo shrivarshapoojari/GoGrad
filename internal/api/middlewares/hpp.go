f9532fa38f6e1b9b52a8f110582403da69402daf
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-09-09 09:15:27 +0530

    update hpp middleware

--- internal/api/middlewares/hpp.go
+++ internal/api/middlewares/hpp.go
@@ -17,3 +17,57 @@ type HPPOptions struct{
 
 
 
+func Hpp(options HPPOptions) func(http.Handler) http.Handler {
+
+	return func(next http.Handler) http.Handler {
+
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+ 
+			if options.CheckQuery && r.Method == http.MethodPost {
+				filterBodyParams(r, options.Whitelist)
+			}
+
+
+
+
+			next.ServeHTTP(w,r)
+
+
+		})
+	}
+}
+
+
+func filterBodyParams(r *http.Request, whitelist []string)  {
+   
+	err :=r.ParseForm()
+
+	if err != nil {
+		fmt.Println("Error parsing form:", err)
+		return
+	}
+
+	for k,v := range r.Form{
+
+		if len(v) > 1{
+			r.Form.Set(k,v[0])
+		}
+		if !isWhiteListed(k,whitelist){
+			 delete (r.Form,k)
+		}
+
+	}
+
+
+}
+
+
+func isWhiteListed(param string, whitelist []string) bool {
+
+	for _, p := range whitelist {
+		if p == param {
+			return true
+		}
+		return  false
+}
